1ca6833f1708b68c50c23c2e7f8159484a615f19
EC2 Default User <ec2-user@ip-172-31-31-162.ap-northeast-1.compute.internal>
2024-10-17 09:34:50 +0000

    view page create genre customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :customers, controllers: {
+  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  devise_for :admin, controllers: {
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-
 
   scope module: :public do
     resources :items, only: [:index, :show]

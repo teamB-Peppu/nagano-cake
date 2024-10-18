f816f9c47abd52b821362b8b0236a00a3a992379
ShutoIto <175019209+shuto822@users.noreply.github.com>
2024-10-18 08:13:36 +0000

    Bootstrapとkaminari

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,4 +8,10 @@ class Public::CustomersController < ApplicationController
   def unsubscribe
   end
 
+  protected
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name, :last_name, :first_name, :kana_last, :kana_first, :postal_code, :address, :telephone_number])
+  end
+
 end

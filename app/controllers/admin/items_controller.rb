4dea77d890c1b8bfb198f27f76319ed851a75c9d
EC2 Default User <ec2-user@ip-172-31-14-26.ap-northeast-1.compute.internal>
2024-10-20 06:33:12 +0000

    new edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,6 +12,7 @@ class Admin::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @item = Item.find(params[:id])
   end
 
   def show
@@ -24,6 +25,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+     redirect_to admin_item_path(@item.id)
+    else
+     render :edit
+    end
   end
 
   private

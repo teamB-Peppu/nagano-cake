120abd4a22cb422738f403c5bd51d6a1bd8c0737
EC2 Default User <ec2-user@ip-172-31-14-26.ap-northeast-1.compute.internal>
2024-10-21 07:26:15 +0000

    item

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,5 @@ class Item < ApplicationRecord
   def add_tax_sales_price
     (self.sales_price * 1.10).round
   end
-  
-  def sales_status
-    if is_active == true
-      "販売中"
-    else
-      "販売停止中"
-    end
-  end
-
-  enum status: { on_sale: 0, off_sale: 1 }
 
 end

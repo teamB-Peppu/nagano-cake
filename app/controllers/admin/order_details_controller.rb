62e7a53aad2fbcef13c209f0f20cd13fc9b983d5
EC2 Default User <ec2-user@ip-172-31-14-26.ap-northeast-1.compute.internal>
2024-10-22 09:10:00 +0000

    status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,23 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+   @order = Order.find(params[:id])
+   @order_details = OrderDetail.find(params[:order_detail][:order_detail_id])
+    if @order_details.update!(order_detail_params)
+     redirect_to admin_order_path(@order.id)
+    else
+     render :show
+    end
+  end
+
+    private
+
+  def order_params
+    params.require(:order).permit(:name, :sipping_cost, :total_payment, :status)
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:price, :amount, :making_status)
+  end
+
 end

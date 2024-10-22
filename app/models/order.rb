7c62cb8ccd2f5727ef1d257ca4e13bb685ff96b2
EC2 Default User <ec2-user@ip-172-31-39-189.ap-northeast-1.compute.internal>
2024-10-22 09:09:57 +0000

    order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   has_many :order_details, dependent: :destroy
+  has_many :items
 
   has_one_attached :image
 
@@ -11,6 +12,28 @@ class Order < ApplicationRecord
 
   def sum_of_order_price
     total_payment - shipping_cost
+  end
+  
+  def add_tax_sales_price
+    order_details.sum(:price) * 1.1
+  end
+  
+  def subtotal
+    item.add_tax_sales_price * amount
+  end
+  
+  def item
+    order_detail = self.order_details.first
+    if order_detail
+      item = order_detail.item
+      return item
+    else
+      return nil
+    end
+  end
+  
+  def amount
+    order_details.sum(:amount)
   end
 
   enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }

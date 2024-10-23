14a03a0a9a0e1cabf0871f7eaa69a82d6936d35a
EC2 Default User <ec2-user@ip-172-31-14-26.ap-northeast-1.compute.internal>
2024-10-23 09:05:32 +0000

    view

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,17 @@ class Item < ApplicationRecord
   def add_tax_sales_price
     (self.price * 1.10).round
   end
+  
+  def status
+    if is_active == true
+      "販売中"
+    else
+      "販売停止中"
+    end
+  end
+  
+  def active_for_authentication?
+    super && (is_active == true)
+  end
 
 end

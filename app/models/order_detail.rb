193125daf8affd56b7d633c5af4811739fb879e7
EC2 Default User <ec2-user@ip-172-31-39-189.ap-northeast-1.compute.internal>
2024-10-26 07:32:02 +0000

    check

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -11,7 +11,7 @@ class OrderDetail < ApplicationRecord
     return add_tax_sales_price * amount
   end
 
-  enum making_status: { no: 0, waiting: 1, production: 2, ok: 3 }
+  enum making_status: { aa: 0, waiting: 1, production: 2, ok: 3 }
 
 
 end
